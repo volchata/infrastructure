c18814fb3afc8e1769f5d6cf48901e38a4c63351
sasha <sasha@localhost.localdomain>
2016-08-27 07:46:04 +0300

    client dependencies

--- provisions.sh
+++ provisions.sh
@@ -1,5 +1,5 @@
 sudo su
-dnf -y install nodejs npm git mocha mongodb-server
+dnf -y install nodejs npm git mocha mongodb-server vim mc
 service mongod start
 mkdir -p /var/www/html/shri/
 chown vagrant:vagrant /var/www/html/shri/
--- user_provisions.sh
+++ user_provisions.sh
@@ -4,6 +4,7 @@ cd  /var/www/html/shri/server
 npm install
 npm test
 mocha test/controllers/user.test.js
+npm run start &
 mkdir -p /var/www/html/shri/client
 git clone https://github.com/volchata/pepo_client.git /var/www/html/shri/client
 cd  /var/www/html/shri/client
@@ -11,6 +12,54 @@ npm install enb
 npm install enb-js
 npm install bower
 npm install grunt
+npm install concat-map
+npm install balanced-match
+npm install readable-stream
+npm install util
+npm install lodash
+npm install caniuse-db
+npm install module-deps
+npm install deps-sort
+npm install browser-pack
+npm install insert-module-globals
+npm install syntax-error
+npm install assert
+npm install buffer
+npm install console-browserify
+npm install constants-browserify
+npm install crypto-browserify
+npm install domain-browser
+npm install events
+npm install stream-http
+npm install https-browserify
+npm install os-browserify
+npm install path-browserify
+npm install punycode
+npm install querystring-es3
+ npm install stream-browserify
+ npm install string_decoder
+ npm install utils
+ npm install timers-browserify
+ npm install tty-browserify
+ npm install url
+ npm install vm-browserify
+ npm install browserify-zlib
+ npm install process
+ npm install labeled-stream-splicer
+ npm install through2
+npm install concat-stream
+npm install inherits
+npm install xtend
+npm install defined
+ npm install defined
+ npm install has
+ npm install htmlescape
+ npm install shasum
+ npm install browser-resolve
+ npm install resolve
+ npm install read-only-stream
+
 npm install
 npm install
-npm test
+npm run make
+npm run start &
